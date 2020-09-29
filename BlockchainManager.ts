2c669721499d66da0f3c941cfc74810ab19d2776
awolfsdorf <awolfsdorf@atixlabs.com>
2020-09-29 10:46:51 -0300

    feat: add first version of the function

--- BlockchainManager.ts
+++ BlockchainManager.ts
@@ -119,6 +119,25 @@ export class BlockchainManager {
     return this.didResolver.resolve(did);
   }
 
+
+  /**
+   * Sets an DID attribute
+   * @param {string}  identity  DID to set the attribute in
+   * @param {string}  key  Attribute key
+   * @param {string}  value  Attribute value
+   * @param {number}  validity
+   */
+  async setAttribute(identity: string, key: string, value: string, validity: number = Constants.BLOCKCHAIN.ATTRIBUTE_VALIDITY) {
+    const didAddr = BlockchainManager.getDidAddress(identity);
+    const options: Options = {from: didAddr, gasPrice: undefined, gas: undefined};
+    const contract = BlockchainManager.getDidContract(options);
+    const keyBytes = web3.utils.fromAscii(key)
+    const valueBytes = web3.utils.fromAscii(value)
+    const setAttrMethod = contract.methods.setAttribute(didAddr, keyBytes, valueBytes, validity);
+    options.gas = await this.getGasLimit(setAttrMethod, options);
+    return setAttrMethod.send(options);
+  }
+
   /**
    * Creates a JWT from a base payload with the information to encode
    * @param {string}  issuerDid  Issuer DID
